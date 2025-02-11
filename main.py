9e9d1a113672cd4963cfc9a7c19740b4b756899d
Mateus B. Marques <mateusbiegermarques@gmail.com>
2025-02-11 14:17:38 -0300

    Ta quase la so falta arrumar algumas coisas e fazer o bicho dar trinca

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import estado
+import random
 
 pygame.init()
 
@@ -20,6 +21,7 @@ casas = {
     22: (100 + offset_x, 700 + offset_y), 23: (400 + offset_x, 700 + offset_y), 24: (700 + offset_x, 700 + offset_y)
 }
 
+#conexoes entre as casas
 conexoes = [
     (1, 2), (2, 3), (1, 10), (3, 15), (10, 22), (15, 24), (22, 23), (23, 24),
     (4, 5), (5, 6), (4, 11), (6, 14), (11, 19), (14, 21), (19, 20), (20, 21),
@@ -27,7 +29,8 @@ conexoes = [
     (2, 5), (5, 8), (10, 11), (11, 12), (13, 14), (14, 15), (17, 20), (20, 23)
 ]
 
-estado_jogo = {i: None for i in casas.keys()}
+#estado das casas, None = vazio | Vermelho | Azul
+estado_casa = {i: None for i in casas.keys()} 
 
 jogador_atual = "Vermelho"
 pecas_colocadas = {"Vermelho": 0, "Azul": 0}
@@ -37,67 +40,93 @@ remocao_pendente = False
 trincas_formadas = {"Vermelho": set(), "Azul": set()}
 peca_selecionada = None
 vencedor = None
-
-
+#---------------------------------------------------------------------------------------------------------------------------------------
+# Função para adicionar uma peça ao tabuleiro
+def adicionar_peca(casa):
+    global jogador_atual, pecas_colocadas, pecas_restantes, pecas_tabuleiro
+    if estado_casa[casa] is None:
+        estado_casa[casa] = jogador_atual
+        pecas_colocadas[jogador_atual] += 1
+        pecas_restantes[jogador_atual] -= 1
+        pecas_tabuleiro[jogador_atual] += 1
+        verificar() # Verifica se o jogador formou uma trinca ou se o oponente perdeu
+#---------------------------------------------------------------------------------------------------------------------------------------
+#Funcao para mover uma peça
+def mover_peca(casa_origem, casa_destino):
+    global jogador_atual, pecas_tabuleiro
+    estado_casa[casa_destino] = jogador_atual
+    estado_casa[casa_origem] = None
+    verificar() # Verifica se o jogador formou uma trinca ou se o oponente perdeu
+#---------------------------------------------------------------------------------------------------------------------------------------
+# Função para verificar se uma trinca foi formada
 def verificar_linha(jogador):
-    trincas = [
+    
+    trincas = [ # Todas as trincas possíveis
         (1, 2, 3), (4, 5, 6), (7, 8, 9), (10, 11, 12), (13, 14, 15), (16, 17, 18), (19, 20, 21), (22, 23, 24),
         (1, 10, 22), (4, 11, 19), (7, 12, 16), (2, 5, 8), (17, 20, 23), (9, 13, 18), (6, 14, 21), (3, 15, 24)
     ]
+    
     for c1, c2, c3 in trincas:
-        if estado_jogo[c1] == estado_jogo[c2] == estado_jogo[c3] == jogador:
-            if (c1, c2, c3) not in trincas_formadas[jogador]:
+        if estado_casa[c1] == estado_casa[c2] == estado_casa[c3] == jogador: # 3 peças do mesmo jogador em uma possivel trinca
+            
+            if (c1, c2, c3) not in trincas_formadas[jogador]: # Verifica se a trinca já foi formada
                 trincas_formadas[jogador].add((c1, c2, c3))
-                return True
-    return False
-
+                return True # Trinca formada
+            
+    return False # Nenhuma trinca formada
+#---------------------------------------------------------------------------------------------------------------------------------------
+# Função para remover uma peça do oponente
 def remover_peca(casa):
     global remocao_pendente, jogador_atual, pecas_tabuleiro
-    if estado_jogo[casa] and estado_jogo[casa] != jogador_atual:
-        estado_jogo[casa] = None
+    if estado_casa[casa] != jogador_atual:
+        estado_casa[casa] = None
         pecas_tabuleiro["Vermelho" if jogador_atual == "Azul" else "Azul"] -= 1
         remocao_pendente = False
         jogador_atual = "Vermelho" if jogador_atual == "Azul" else "Azul"
-
+#---------------------------------------------------------------------------------------------------------------------------------------
+# Função para verificar se o oponente perdeu
 def verificar_derrota(jogador):
     if pecas_restantes[jogador] > 0: # jogador ainda nao colocou todas as pecas
         return False
     
     if pecas_tabuleiro[jogador] < 3: # jogador tem menos de 3 pecas
         return True
-    
-
+#---------------------------------------------------------------------------------------------------------------------------------------
+# Função para desenhar as setas quando o jogador quer mover uma peça
 def desenhar_setas(casa):
     cor_seta = (255, 0, 0) if jogador_atual == "Vermelho" else (0, 0, 255)
     for c1, c2 in conexoes:
-        if c1 == casa and estado_jogo[c2] is None:
+        if c1 == casa and estado_casa[c2] is None:
             pos2 = casas[c2]
             pygame.draw.polygon(screen, cor_seta, [
                 (pos2[0] - 10, pos2[1] - 10), 
                 (pos2[0] + 10, pos2[1] - 10), 
                 (pos2[0], pos2[1] + 10)
             ])
-        elif c2 == casa and estado_jogo[c1] is None:
+        elif c2 == casa and estado_casa[c1] is None:
             pos2 = casas[c1]
             pygame.draw.polygon(screen, cor_seta, [
                 (pos2[0] - 10, pos2[1] - 10), 
                 (pos2[0] + 10, pos2[1] - 10), 
                 (pos2[0], pos2[1] + 10)
             ])
-            
-def verificar(): # Verifica se o jogador atual formou uma trinca ou se o oponente perdeu
+#---------------------------------------------------------------------------------------------------------------------------------------  # Todas as verificações de vitória e trinca em um único lugar       
+def verificar(): 
     global remocao_pendente, vencedor, jogador_atual
-    if verificar_linha(jogador_atual):
+    if verificar_linha(jogador_atual): # Verifica se o jogador formou uma trinca
         remocao_pendente = True
-    if verificar_derrota("Vermelho" if jogador_atual == "Azul" else "Azul"):
+        
+    if verificar_derrota("Vermelho" if jogador_atual == "Azul" else "Azul"): # Verifica se o oponente perdeu
         vencedor = jogador_atual
-        rodando = False
         tela_vitoria(vencedor)
-    if not remocao_pendente:
+        rodando = False
+        
+    if not remocao_pendente: # Se nenhuma trinca foi formada, passa a vez
         jogador_atual = "Vermelho" if jogador_atual == "Azul" else "Azul"
-
+#---------------------------------------------------------------------------------------------------------------------------------------
+# Função para o algoritmo A*
 def algoritmo_a_estrela():
-    estado_atual = estado.Estado(estado_jogo, "Azul")  # Corrigido para chamar a classe Estado
+    estado_atual = estado.Estado(estado_casa, "Azul")  # Corrigido para chamar a classe Estado
 
     # Primeiro, tentar bloquear
     caminho_bloquear = estado_atual.a_star_search(
@@ -135,7 +164,8 @@ def algoritmo_a_estrela():
         # Se ambos são igualmente viáveis ou nenhum encontrado, fazer um movimento aleatório
         for movimento in estado_atual.movimentos_possiveis(conexoes, "Azul"):
             return movimento
-
+#---------------------------------------------------------------------------------------------------------------------------------------
+# Função para o menu principal
 def menu_principal():
     while True:
         screen.fill((0, 0, 0))
@@ -161,7 +191,8 @@ def menu_principal():
                     text = font.render(modo, True, (255, 255, 255))
                     if (WIDTH // 2 - text.get_width() // 2 <= mx <= WIDTH // 2 + text.get_width() // 2) and (HEIGHT // 2 + i * 50 <= my <= HEIGHT // 2 + i * 50 + text.get_height()):
                         return modo
-
+#---------------------------------------------------------------------------------------------------------------------------------------
+# Função para a tela de vitória
 def tela_vitoria(vencedor):
     while True:
         screen.fill((0, 0, 0))
@@ -185,7 +216,8 @@ def tela_vitoria(vencedor):
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 if button_rect.collidepoint(event.pos):
                     return
-
+#---------------------------------------------------------------------------------------------------------------------------------------
+# Função principal do jogo
 def jogo(modo_jogo):
     global jogador_atual, peca_selecionada, remocao_pendente, vencedor, rodadas
     rodando = True
@@ -200,7 +232,7 @@ def jogo(modo_jogo):
 
         # Desenhar as casas e as peças
         for casa, pos in casas.items():
-            cor = (0, 255, 0) if estado_jogo[casa] is None else (255, 0, 0) if estado_jogo[casa] == "Vermelho" else (0, 0, 255)
+            cor = (0, 255, 0) if estado_casa[casa] is None else (255, 0, 0) if estado_casa[casa] == "Vermelho" else (0, 0, 255)
             pygame.draw.circle(screen, cor, pos, 20)
             font = pygame.font.Font(None, 24)
             text = font.render(str(casa), True, (0, 0, 0))
@@ -233,7 +265,7 @@ def jogo(modo_jogo):
             screen.blit(text, (50, HEIGHT // 2))
             button_text = font.render("Continuar", True, (255, 255, 255))
             button_rect = button_text.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 150))
-            pygame.draw.rect(screen, (100, 100, 100), button_rect.inflate(20, 10))
+            pygame.draw.rect(screen, (50, 100, 100), button_rect.inflate(20, 10))
             screen.blit(button_text, button_rect)
 
         pygame.display.flip()
@@ -254,97 +286,42 @@ def jogo(modo_jogo):
                         if remocao_pendente:
                             remover_peca(casa) # Remove a peça do oponente
                             
-                        elif estado_jogo[casa] == jogador_atual and pecas_colocadas[jogador_atual] == 9:
+                        elif estado_casa[casa] == jogador_atual and pecas_restantes[jogador_atual] == 0:
                             peca_selecionada = casa # Seleciona a peça
                             
-                        elif estado_jogo[casa] is None and peca_selecionada is not None:
+                        elif estado_casa[casa] is None and peca_selecionada is not None:
                             if (peca_selecionada, casa) in conexoes or (casa, peca_selecionada) in conexoes:
-                                estado_jogo[casa] = jogador_atual
-                                estado_jogo[peca_selecionada] = None
-                                peca_selecionada = None
-                                pecas_tabuleiro[jogador_atual] += 1
-                                verificar() # Verifica se o jogador formou uma trinca ou se o oponente perdeu
+                                mover_peca(peca_selecionada, casa) # Move a peça
                                 
                                     
-                        elif estado_jogo[casa] is None and pecas_colocadas[jogador_atual] < 9:
-                            estado_jogo[casa] = jogador_atual
-                            pecas_colocadas[jogador_atual] += 1
-                            pecas_restantes[jogador_atual] -= 1
-                            pecas_tabuleiro[jogador_atual] += 1
+                        elif estado_casa[casa] is None and pecas_restantes[jogador_atual] > 0:
+                            adicionar_peca(casa) # Adiciona a peça
                             
-                            verificar() # Verifica se o jogador formou uma trinca ou se o oponente perdeu
 
         # Lógica para o modo "Humano vs A*"
         if modo_jogo == "Humano vs A*" and jogador_atual == "Azul" and vencedor is None:
+            
             if pecas_restantes["Azul"] > 0: # Se o jogador ainda não colocou todas as peças
-                for casa, ocupado in estado_jogo.items():
-                    if ocupado is None: # Coloca a peça em uma casa vazia
-                        estado_jogo[casa] = jogador_atual
-                        pecas_colocadas[jogador_atual] += 1
-                        pecas_restantes[jogador_atual] -= 1
-                        pecas_tabuleiro[jogador_atual] += 1
-                        verificar() # Verifica se o jogador formou uma trinca ou se o oponente perdeu
-                        break
-            else:
+                # Seleciona uma casa aleatória para adicionar a peça
+                colocou = False
+                while not colocou:
+                    casa = random.choice(list(estado_casa.keys()))
+                    if estado_casa[casa] is None:
+                        adicionar_peca(casa)
+                        colocou = True
+                        
+            else: # Se ja colocou todas as peças
                 # Executa o algoritmo A*
                 movimento = algoritmo_a_estrela()
                 print("Movimento:", movimento)
                 if movimento != (None, None):
                     casa_origem, casa_destino = movimento
-                    estado_jogo[casa_destino] = "Azul"
-                    estado_jogo[casa_origem] = None
+                    estado_casa[casa_destino] = "Azul"
+                    estado_casa[casa_origem] = None
                     pecas_colocadas["Azul"] += 1  # Atualize conforme a lógica do movimento
                     # Verificar trinca e derrota
                     verificar()
                         
-        # Lógica para o modo "Humano vs Minimax"
-        elif modo_jogo == "Humano vs Minimax" and jogador_atual == "Azul" and vencedor is None:
-            if pecas_restantes["Azul"] > 0:
-                for casa, ocupado in estado_jogo.items():
-                    if ocupado is None:
-                        estado_jogo[casa] = "Azul"
-                        pecas_colocadas["Azul"] += 1
-                        pecas_restantes["Azul"] -= 1
-                        verificar()
-                        break
-            else:
-                casa_origem, casa_destino = algoritmo_minimax()
-                if casa_origem is not None and casa_destino is not None:
-                    estado_jogo[casa_destino] = "Azul"
-                    estado_jogo[casa_origem] = None
-                    verificar()
-        # Lógica para o modo "A* vs Minimax"
-        elif modo_jogo == "A* vs Minimax" and vencedor is None:
-            if jogador_atual == "Vermelho":
-                if pecas_restantes["Vermelho"] > 0:
-                    for casa, ocupado in estado_jogo.items():
-                        if ocupado is None:
-                            estado_jogo[casa] = "Vermelho"
-                            pecas_colocadas["Vermelho"] += 1
-                            pecas_restantes["Vermelho"] -= 1
-                            verificar()
-                            break
-                else:
-                    casa_origem, casa_destino = algoritmo_a_estrela()
-                    if casa_origem is not None and casa_destino is not None:
-                        estado_jogo[casa_destino] = "Vermelho"
-                        estado_jogo[casa_origem] = None
-                        verificar()
-            else:
-                if pecas_restantes["Azul"] > 0:
-                    for casa, ocupado in estado_jogo.items():
-                        if ocupado is None:
-                            estado_jogo[casa] = "Azul"
-                            pecas_colocadas["Azul"] += 1
-                            pecas_restantes["Azul"] -= 1
-                            verificar()
-                            break
-                else:
-                    casa_origem, casa_destino = algoritmo_minimax()
-                    if casa_origem is not None and casa_destino is not None:
-                        estado_jogo[casa_destino] = "Azul"
-                        estado_jogo[casa_origem] = None
-                        verificar()
 
     if vencedor:
         tela_vitoria(vencedor)
